1d9d05f395ff649beb69c4a4aac12f85aeb932f6
LMArantes <lucas.marantes@proton.me>
2025-03-03 05:47:07 -0500

    Main structure of the website

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+
 # Historical Ciphers
 caesar_cipher = st.Page("historical-ciphers/caesar_cipher.py", title="Caesar Cipher")
 vigenere_cipher = st.Page("historical-ciphers/vigenere_cipher.py", title="Vigenere Cipher")
@@ -15,10 +16,36 @@ sha_2 = st.Page("cryptographic-hash-functions/sha_2.py", title="SHA-2")
 sha_3 = st.Page("cryptographic-hash-functions/sha_3.py", title="SHA-3")
 blake = st.Page("cryptographic-hash-functions/blake.py", title="BLAKE")
 
+
+# Modern Symmetric-Key Algorithms (Block Ciphers)
+des = st.Page("modern-symmetric-key-algorithms/block-ciphers/data_encryption_standard.py", title="DES")
+triple_des = st.Page("modern-symmetric-key-algorithms/block-ciphers/triple_des.py", title="3DES")
+aes = st.Page("modern-symmetric-key-algorithms/block-ciphers/advanced_encryption_standard.py", title="AES")
+blowfish = st.Page("modern-symmetric-key-algorithms/block-ciphers/blowfish.py", title="Blowfish")
+twofish = st.Page("modern-symmetric-key-algorithms/block-ciphers/twofish.py", title="Twofish")
+rc6 = st.Page("modern-symmetric-key-algorithms/block-ciphers/r6.py", title="RC6")
+
+
+# Modern Symmetric-Key Algorithms (Stream Ciphers)
+rc4 = st.Page("modern-symmetric-key-algorithms/stream-ciphers/rc4.py", title="RC4")
+chacha20 = st.Page("modern-symmetric-key-algorithms/stream-ciphers/chacha20.py", title="ChaCha20")
+salsa20 = st.Page("modern-symmetric-key-algorithms/stream-ciphers/salsa20.py", title="Salsa20")
+
+
+# Asymmetric Encryption (Public-key cryptography)
+rsa = st.Page("asymmetric-encryption/rivest_shamir_adleman.py", title="RSA")
+dh = st.Page("asymmetric-encryption/diffie_hellman.py", title="Diffie-Hellman (DH)")
+ecc = st.Page("asymmetric-encryption/elliptic_curve_cryptography.py", title="Elliptic Curve Cryptography (ECC)")
+elgamal = st.Page("asymmetric-encryption/elgamal.py", title="ElGamal")
+
+
 pg = st.navigation(
     {
         "Historical Ciphers": [caesar_cipher, vigenere_cipher, transposition_cipher, playfair_cipher, enigma_machine],
-        "Cryptographic Hash Functions: ": []
+        "Cryptographic Hash Functions": [md5, sha_1, sha_2, sha_3, blake],
+        "Block Ciphers": [des, triple_des, aes, blowfish, twofish, rc6],
+        "Stream Ciphers": [rc4, chacha20, salsa20],
+        "Public-key Cryptography": [rsa, dh, ecc, elgamal]
     }
 )
 
